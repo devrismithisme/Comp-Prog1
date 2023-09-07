6cea0e097b45fdff96cd4f36d7c72cb5b6d88fcb
Scholar <scholar@paradigm.local>
2023-09-07 13:31:07 -0600

    intro to key concepts

---
+++ introkeyconcepts.py
@@ -0,0 +1,49 @@
+my_name = "Miss Devri"
+
+def give_a_name():
+    a_name = "William"
+    print(a_name) 
+
+
+print(my_name) 
+
+user_input = input("What is your name? ")
+print(f"Nice to meet you, {user_input}!")
+
+my_string = "This is my string"
+my_integer = 1456
+my_float = 3.14 #a float has a decimal point
+is_my_boolean = True 
+
+print(my_integer + my_float)
+
+my_declaration =""
+my_initialization = "Hello World" 
+
+add = 1 + 2 #arithmetic operators
+subtract = 2 - 1
+divide = 2 / 1
+multiply = 1 * 3
+modulo = 27 % 2
+exponent = 2 ** 3
+
+print(modulo)
+
+statement = 4 > 2 #greater than
+statement1 = 4 < 2 #LESS THAN
+statement2 = 4 >= 2 #greater than or equal to
+statement3 = 4 <= 2 #less than or equal to
+statement4 = 4 == 2 #is it equal?
+statement5 = 4 != 2 #! is the bang. Not equal to. 
+
+print(statement)
+
+message = 4 > 2 and 33 < 43 #and
+message2 = 4 < 2 or 33 < 43 #or, just need one to be true
+
+print(message)
+
+x = False
+
+print(not x) #you should get the opposite of x
+
