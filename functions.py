60b19fc7f849e4922d874e33fabbf880295fc3bb
Scholar <scholar@paradigm.local>
2023-09-21 13:33:58 -0600

    functions

--- functions.py
+++ functions.py
@@ -3,14 +3,9 @@ name = "Devri Anne Smith"
 print(name)
 
 
-
-def myfunc():
-     print("My name is {name} and + {phrase1} + {animal1}")
-
 user_name = input
 
 def input():
      question = input("What is your name? ")
      response = ("Nice to meet you{user_name}") 
 
-print(def input) 
