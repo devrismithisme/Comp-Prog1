de4d11f1f9fd123b81b66dcad7b94540e3c68ce1
Scholar <scholar@paradigm.local>
2023-09-26 13:32:40 -0600

    if statements

---
+++ if.py
@@ -0,0 +1,20 @@
+
+current_salary = int(input("Please tell me your current salary for our company. "))
+year = int(input("How many years have you worked with our company? "))
+if year >= 5:
+     print(f"Becuase you have worked with this company for more than five years, you get a bonus of ${current_salary * .05} ")
+else:
+     print("I'm sorry, you do not get a bonus. Work with us for more than five years and you will get this bonus.")
+
+rectangle_length = (input("What is the length of your rectangle? "))
+rectangle_breadth = (input("How wide is your rectangle? "))
+if rectangle_length == rectangle_breadth:
+    print("I'm sorry, but your rectangle is actually a square")
+else:
+    print("Congratulations! You have a rectangle!")
+
+
+number1 = int(input("Please enter a number "))
+number2 = int(input("Please enter another number "))
+
+
