a5f989dac97b6029895329899048a10077eee03d
Scholar <scholar@paradigm.local>
2023-10-12 12:42:24 -0600

    fibonacci sequence, altered

---
+++ chooseadventure.py
@@ -0,0 +1,9 @@
+question1 = input("Hello. Welcome to Choose Your Own Adventure. Would you like to play? ") 
+if question1 in ("Yes" or "yes)"):
+    print("Welcome player")
+    question2 = input("What is your name player? ")
+    question3 = "Would you like to: 1. go to Walmart or 2. go camping"
+    if question3 in "1":
+        print("You walk into Walmart to buy some chapstik. Unfortunately, they have run out of chapstik.")
+        print("You walk around the store, awkward and uncertain.")
+        print("Time flies and it is suddenly 6:00, closing time.")
--- fibonacci.py
+++ fibonacci.py
@@ -16,4 +16,19 @@ else:
 
 
 
-    
+how_deep = int(input("How deep into the fibonacci sequence would you like to go?"))
+
+x = 0
+y = 1
+z = y
+count = 1
+
+while count <= how_deep:
+    print(f"{z} ")
+    count += 1
+    x, y = y, z
+    z = x + y
+
+
+
+
