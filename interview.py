24145860f600c532bfe441dbb17eeef333107377
Scholar <scholar@paradigm.local>
2023-10-31 13:34:19 -0600

    interview

---
+++ interview.py
@@ -0,0 +1,15 @@
+St1 = "Hello and welcome to your job interview for programming. My name is Maxwell."
+print(St1)
+Q1 = input("What is your name? ")
+Q2 = input(f"It's nice to meet you {Q1}. How long have you been programming? Whether in highschool, college, personally, or anytime else. ")
+
+if Q2 < 4:
+    print("Oh. I am afraid we are only looking for employees who have done programming for 4 or more years.")
+else:
+    print("Your resume says that you have worked with a number of programming platforms.")
+    Q3 = input("Which of these platforms do you think you worked in the best? ")
+    if Q3 in "python" or "Python":
+        print("Here at _______ we work a lot with codes that are similar to python.")
+        Q4 = 
+    else:
+        print("We are looking to hire ")s
