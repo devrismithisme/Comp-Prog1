2613170302a6f65152229d14f22cb48e7ac0d39c
Scholar <scholar@paradigm.local>
2023-11-14 13:34:41 -0700

    guess a word

---
+++ geussword.py
@@ -0,0 +1,79 @@
+
+letter.dict = {
+    "A" or "a"
+     "B" or "b"
+     "C" or "c"
+      "D" or "d"
+      "E" or "e"
+      "F" or "f"
+      "G" or "g"
+      
+}
+
+
+
+Greet1 = "Hello and welcome to Geuss the Word!"
+print(Greet1)
+Q1 = input("Would you like to play? ")
+if Q1 in "Yes" or "yes":
+    print("Great!")
+else:
+    print("Goodbye then")
+
+print("The word is 1 letter long: _")
+Request = input("Geuss a letter:")
+if Request in "I" or "i":
+    print("Correct: I")
+if Request in:
+    print("Incorrect!")
+
+
+
+
+
+
+print("The word is six letters long.  _ _ _ _ _ _ ")
+
+
+
+
+
+
+
+
+
+
+Request1 = input("Geuss a letter: ")
+if Request1 in "h" or "H":
+    print("Correct!: H _ _ _ _ _")
+if Request1 in "l" or "L" + "h" or "H":
+    print("Correct!: H _ _ _ L _")
+if Request1 in "l" or "L" + "h" or "H" + "A" or "a":
+    print("Correct!: H A _ _L _")
+
+Request2 = input("Geuss a letter: ")
+if Request2 in "a" or "A":
+    print("Correct!: _ A _ _ _ _")
+
+Request3 = input("Geuss a letter: ")
+if Request3 in "n" or "N":
+    print("Correct: _ _ N _ _ _")
+
+Request4 = input("Geuss a letter: ")
+if Request4 in "d" or "D":
+    print("Correct!:_ _ _ D _ _")
+
+Request5 = input("Geuss a letter: ")
+if Request5 in "L" or "l":
+    print("Correct!: _ _ _ _ L _")
+
+Request6 = input("Geuss a letter: ")
+if Request6 in "E" or "e":
+    print("Correct!:_ _ _ _ _ E")
+    
+
+
+
+
+#
+
